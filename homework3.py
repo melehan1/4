9f310e2e99037d7e6ab2133d7f8a09a6935c2627
Дмитрий <melehan@mail.ru>
2024-05-20 08:44:26 +0300

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,9 @@
+name = 'Дмитрий'
+print(name, (type(name)))
+age = 49
+print(age, (type(age)))
+age = (age + 1)
+print(age)
+is_student = True
+is_student = (49 < age)
+print(is_student, (type(is_student)))
